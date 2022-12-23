89ab3379d86686a095d3d2c3a59f4b84af6ae600
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2022-12-23 18:31:44 +0100

    Revert "Work"
    This reverts commit f5c6343e2964da117585e6e250cadadc154e64cb.

--- Coroutine.lua
+++ Coroutine.lua
@@ -1,67 +1,59 @@
-local addOnName = ...
-local version = '1.0.0'
+Coroutine = {}
 
-if not Library.isRegistered(addOnName, version) then
-  --- @class Coroutine
-  local Coroutine = {}
+function Coroutine.runAsCoroutine(fn)
+  local thread = coroutine.create(fn)
+  RunNextFrame(function ()
+    Coroutine.resumeWithShowingError(thread)
+  end)
+  return thread
+end
 
-  Library.register(addOnName, version, Coroutine)
-
-  function Coroutine.runAsCoroutine(fn)
-    local thread = coroutine.create(fn)
-    RunNextFrame(function ()
-      Coroutine.resumeWithShowingError(thread)
-    end)
-    return thread
+function Coroutine.resumeWithShowingError(thread, ...)
+  local result = { coroutine.resume(thread, ...) }
+  local wasSuccessful = result[1]
+  if not wasSuccessful then
+    local errorMessage = result[2]
+    error(errorMessage .. '\n' .. debugstack(thread), 0)
   end
+  return unpack(result)
+end
 
-  function Coroutine.resumeWithShowingError(thread, ...)
-    local result = { coroutine.resume(thread, ...) }
-    local wasSuccessful = result[1]
-    if not wasSuccessful then
-      local errorMessage = result[2]
-      error(errorMessage .. '\n' .. debugstack(thread), 0)
+function Coroutine.waitFor(predicate, timeout)
+  local thread = coroutine.running()
+  local ticker
+  local startTime = GetTime()
+  ticker = C_Timer.NewTicker(0, function()
+    if predicate() then
+      ticker:Cancel()
+      Coroutine.resumeWithShowingError(thread, true)
+    elseif timeout and GetTime() - startTime >= timeout then
+      ticker:Cancel()
+      Coroutine.resumeWithShowingError(thread, false)
     end
-    return unpack(result)
-  end
+  end)
+  return coroutine.yield()
+end
 
-  function Coroutine.waitFor(predicate, timeout)
-    local thread = coroutine.running()
-    local ticker
-    local startTime = GetTime()
-    ticker = C_Timer.NewTicker(0, function()
-      if predicate() then
-        ticker:Cancel()
-        Coroutine.resumeWithShowingError(thread, true)
-      elseif timeout and GetTime() - startTime >= timeout then
-        ticker:Cancel()
-        Coroutine.resumeWithShowingError(thread, false)
-      end
-    end)
-    return coroutine.yield()
-  end
+Coroutine.waitUntil = Coroutine.waitFor
 
-  Coroutine.waitUntil = Coroutine.waitFor
+function Coroutine.waitForDuration(duration)
+  local thread = coroutine.running()
+  C_Timer.After(duration, function()
+    Coroutine.resumeWithShowingError(thread)
+  end)
+  return coroutine.yield()
+end
 
-  function Coroutine.waitForDuration(duration)
-    local thread = coroutine.running()
-    C_Timer.After(duration, function()
-      Coroutine.resumeWithShowingError(thread)
-    end)
-    return coroutine.yield()
-  end
+function Coroutine.runNextFrame(fn)
+  RunNextFrame(function ()
+    Coroutine.runAsCoroutine(fn)
+  end)
+end
 
-  function Coroutine.runNextFrame(fn)
-    RunNextFrame(function ()
-      Coroutine.runAsCoroutine(fn)
-    end)
-  end
-
-  function Coroutine.yieldAndResume()
-    local thread = coroutine.running()
-    C_Timer.After(0, function ()
-      Coroutine.resumeWithShowingError(thread)
-    end)
-    coroutine.yield()
-  end
+function Coroutine.yieldAndResume()
+  local thread = coroutine.running()
+  C_Timer.After(0, function ()
+    Coroutine.resumeWithShowingError(thread)
+  end)
+  coroutine.yield()
 end
